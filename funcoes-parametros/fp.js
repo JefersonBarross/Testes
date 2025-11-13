fa298be4d397e26b742d97a5f67b15d693adbca3
JefersonBarross <jefersoncb30@gmail.com>
2025-11-12 21:22:58 -0300

    Add files via upload

---
+++ funcoes-parametros/fp.js
@@ -0,0 +1,39 @@
+function soma(){
+    const v1 = document.getElementById("valor1").value;
+    const v2 = document.getElementById("valor2").value;
+
+    const soma = Number(v1) + Number(v2) 
+
+    alert(`O Resultado da soma é ${soma}`)
+}
+
+function sub(){
+    const v1 = document.getElementById("valor1").value;
+    const v2 = document.getElementById("valor2").value;
+
+    const sub = Number(v1) - Number(v2) 
+
+    alert(`O Resultado da soma é ${sub}`)
+}
+
+function divi(){
+    const v1 = document.getElementById("valor1").value;
+    const v2 = document.getElementById("valor2").value;
+
+    const divi = Number(v1) / Number(v2) 
+
+    alert(`O Resultado da soma é ${divi}`)
+}
+
+function mul(){
+    const v1 = document.getElementById("valor1").value;
+    const v2 = document.getElementById("valor2").value;
+
+    const mul = Number(v1) * Number(v2) 
+
+    alert(`O Resultado da soma é ${mul}`)
+}
+
+
+
+
