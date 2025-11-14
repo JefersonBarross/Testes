ac7085846fe198d1f5414d87270e8aa4b60aa556
JefersonBarross <jefersoncb30@gmail.com>
2025-11-13 21:18:50 -0300

    Add files via upload

---
+++ darkMode/darkMode.js
@@ -0,0 +1,10 @@
+function dark(){
+    document.body.style.background = 'black';
+    document.body.style.color = 'white';
+}
+
+function light(){
+    document.body.style.background = 'white';
+    document.body.style.color = 'black';
+}
+
---
+++ objetojs/objetoJs.js
@@ -0,0 +1,10 @@
+let pessoa = {
+    nome: "Jeff",
+    idade: 19,
+    altura: 173,
+    peso: 72
+}
+
+console.log("Meu nome é " + pessoa.nome + " tenho " + pessoa.idade + " anos Minha altura é " + pessoa.altura + " e peso "+pessoa.peso+ "kg" ) 
+
+alert(`Meu nome é ${pessoa.nome} Tenho ${pessoa.idade} anos Minha altura é ${pessoa.altura} e meu peso é ${pessoa.peso}kg`)
